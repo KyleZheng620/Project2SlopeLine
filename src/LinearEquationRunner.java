8002b70fc0f7f93800d1d1f13a064c713e6aea1a
BT_1N3_11 <sample@bths.edu>
2024-10-25 14:03:24 -0400

    Project Final commit

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -2,7 +2,6 @@ public class LinearEquationRunner {
     public static void main(String[] args) {
         LinearEquationLogic equation = new LinearEquationLogic();
         equation.start();
-
-
+        System.out.println("I'm done with the project already!");
     }
 }
